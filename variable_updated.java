3cdfa7a198a4a22b70da7a551e8f96dd32df70e6
Ishmam Bin Rofi <113937167+ishmam-br10@users.noreply.github.com>
2023-02-06 23:37:11 +0600

    Add files via upload

---
+++ variable_updated.java
@@ -0,0 +1,14 @@
+public class variable_updated {
+    public static void main(String[] args) {
+        String Name = "Ishmam";
+        String Job = "Bekar";
+        String NameCopy = Name;
+        Name = "Mew";
+        System.out.print("Printing variable , Name: ");
+        System.out.println(Name);
+        System.out.print("Printing variable NameCopy: ");
+        System.out.println(NameCopy);
+        System.out.println(Job);
+
+    }
+}
