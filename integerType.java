277cf404694ae71ab4348f61afa221f994d0bb1f
Ishmam Bin Rofi <113937167+ishmam-br10@users.noreply.github.com>
2023-02-20 20:10:05 +0600

    Add files via upload

---
+++ integerType.java
@@ -0,0 +1,17 @@
+public class integerType {
+    public static void main(String[] args) {
+        byte b1 = 5;
+        short s1 = 20;
+        int i1 = 100;
+        long l1 = 9999999999999999L; // L indicates it's a long
+        System.out.println(b1);
+        System.out.println(s1);
+        System.out.println(i1);
+        System.out.println(l1);
+        System.out.println(b1 - s1);
+        System.out.println(s1 - i1);
+        System.out.println(l1 - s1);
+
+
+    }
+}
