c1393da652dbbc59789755b577e5e220eef9cb63
Ishmam Bin Rofi <113937167+ishmam-br10@users.noreply.github.com>
2023-02-23 20:51:12 +0600

    Add files via upload

---
+++ double_float.java
@@ -0,0 +1,10 @@
+public class double_float {
+    public static void main(String[] args) {
+        double d1 = 10.3444D;
+        float f1 = 10.5F;
+        double d2 = 10.4F;
+        System.out.println(d1);
+        System.out.println(f1);
+        System.out.println(d2);
+    }
+}
