647462a870362ca08dfc59aaa9e2bb7b15d974c2
Ishmam Bin Rofi <113937167+ishmam-br10@users.noreply.github.com>
2023-02-25 20:13:12 +0600

    Add files via upload

---
+++ favorite_number_exercise.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class favorite_number_exercise {
+    public static void main(String[] args) {
+        Scanner in1 = new Scanner(System.in);
+        System.out.print("Enter your favorite integer: ");
+        System.out.println(in1.nextInt() + " Is my favorite integer too !");
+    }
+}
