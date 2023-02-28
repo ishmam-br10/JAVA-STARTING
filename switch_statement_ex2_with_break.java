57635ced35b0511af319a33f9b5c69151186e040
Ishmam Bin Rofi <113937167+ishmam-br10@users.noreply.github.com>
2023-02-28 12:06:10 +0600

    Add files via upload

---
+++ switch_statement_ex2_with_break.java
@@ -0,0 +1,22 @@
+public class switch_statement_ex2_with_break {
+    public static void main(String[] args) {
+        Integer x = 30;
+        // switch case without break
+        switch(x){
+            case 10:
+                System.out.println("=10");
+                break;
+            case 20:
+                System.out.println("=20");
+                break;
+            case 30:
+                System.out.println("=30");
+                break;
+            default:
+                System.out.println("none of the avobe");
+                break;
+
+        }
+
+    }
+}
