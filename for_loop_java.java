53a43b5d349953e8c0fb0dd263ef44d5ab6a45cd
Ishmam Bin Rofi <113937167+ishmam-br10@users.noreply.github.com>
2023-03-02 02:55:17 +0600

    Add files via upload

---
+++ for_loop_java.java
@@ -0,0 +1,8 @@
+public class for_loop_java {
+    public static void main(String[] args) {
+        for(int i = 1; i <=5 ; i++){
+            System.out.println("Fuck you! ");
+        }
+    }
+    
+}
