dc96d43d1227d6bb2d530cbf3ad3a16686fe8fc8
Ishmam Bin Rofi <113937167+ishmam-br10@users.noreply.github.com>
2023-03-02 02:55:06 +0600

    Add files via upload

---
+++ while_loop_using_for_loop.java
@@ -0,0 +1,9 @@
+public class while_loop_using_for_loop {
+    public static void main(String[] args) {
+        int i = 1;
+        for(;i<=5;){
+            System.out.println("F*ck you");
+            i++;
+        }
+    }
+}
