f95848587ad5da2199e4f2059851b6b383cd022a
Ishmam Bin Rofi <113937167+ishmam-br10@users.noreply.github.com>
2023-03-02 17:20:17 +0600

    Add files via upload

---
+++ self_curiosity_1.java
@@ -0,0 +1,13 @@
+public class self_curiosity_1 {
+    public static void main(String[] args) {
+
+        for(int x = 1; x<=10; x++){
+            
+            if(x%2 == 0){
+                continue;
+            }
+            System.out.println("The number is "+ x);
+        }
+    }
+    
+}
