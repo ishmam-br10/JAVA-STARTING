a8c69df0a7b47cb6414596884dedd86eb7b12397
ishmam-br10 <ishmam.bin.rofi1@g.bracu.ac.bd>
2023-04-26 21:57:06 +0600

    VS code

--- two_dimensional_array.java
+++ two_dimensional_array.java
@@ -2,5 +2,8 @@ public class two_dimensional_array {
     public static void main(String[] args) {
         int[][] numbers = {{1, 0} ,{2, 5}};
         System.out.println(numbers[0][1]);
+        // System.out.println(numbers);
     }
+    // so array is just like python dictionary
+
 }
