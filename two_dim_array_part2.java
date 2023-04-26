8bb96ddbafae2d44441cb92b7d7a7b3e5c0c9687
ishmam-br10 <ishmam.bin.rofi1@g.bracu.ac.bd>
2023-04-26 22:17:00 +0600

    new file:   two_dim_array_part2.java

---
+++ two_dim_array_part2.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class two_dim_array_part2 {
+    public static void main(String[] args) {
+        int[][] integers = {{1, 3, 5}, {7, 8, 9}};
+        // System.out.println(Arrays.toString(integers));
+        PrintRowbyRow(integers);
+        PrintColumnbyColumn(integers);
+    }
+    public static void PrintRowbyRow(int[][] integers) {
+        System.out.println("Print Row by Row: ");
+        for(int i = 0; i<2; i++){
+            for(int j=0; j< 3; j++){
+                System.out.print(integers[i][j] + " ");
+            }
+        System.out.println();
+        }
+    }
+
+    public static void PrintColumnbyColumn(int[][] integers) {
+        System.out.println("Print Column by Column :");
+        for(int i = 0; i< 3; i++){
+            for(int j =0; j<2; j++){
+                System.out.print(integers[j][i]+" ");
+            }
+        System.out.println();
+        }
+    }
+}
